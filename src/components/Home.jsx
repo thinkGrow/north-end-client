60efb214637c02de681cd85359d267c91cfacf01
MaishaTahsin <rubaitreshad@gmail.com>
2025-07-12 17:36:50 +0600

    create Get API for coffee data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const coffees = useLoaderData();
+  console.log(coffees);
+  return <div></div>;
 };
 
 export default Home;
