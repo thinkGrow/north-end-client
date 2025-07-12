5a3cd7dffe7fcc8b28ba5313ffbe9620e2911b63
MaishaTahsin <rubaitreshad@gmail.com>
2025-07-12 19:48:56 +0600

    delete coffee - Home

---
+++ src/components/CoffeeCard.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Swal from "sweetalert2";
+
+const CoffeeCard = ({ coffee }) => {
+  const { _id, name, price, quantity, photo } = coffee;
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        //start deleting the coffee
+        fetch(`http://localhost:3000/coffees/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your coffee has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
+  return (
+    <div className="card card-side bg-base-100 shadow-sm border-2">
+      <figure>
+        <img src={photo} alt="Movie" />
+      </figure>
+      <div className="flex w-full mt-8 justify-around">
+        <div>
+          <h2>{name}</h2>
+          <p>{price}</p>
+          <p>{quantity}</p>
+        </div>
+        <div className="card-actions justify-end">
+          <div className="join join-vertical space-y-2">
+            <button className="btn join-item">View Details</button>
+            <button className="btn join-item">Edit</button>
+            <button onClick={() => handleDelete(_id)} className="btn join-item">
+              X
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoffeeCard;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import CoffeeCard from "./CoffeeCard";
 
 const Home = () => {
   const coffees = useLoaderData();
   console.log(coffees);
-  return <div></div>;
+  return (
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        {coffees.map((coffee) => {
+          return <CoffeeCard coffee={coffee} key={coffee._id}></CoffeeCard>;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
